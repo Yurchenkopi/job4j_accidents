341ec9535ec1071f4ab49b6538d0602ba0d6c578
Pavel <heppie@yandex.ru>
2024-07-27 11:06:53 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,12 +1,17 @@
 package ru.job4j.accidents.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping({"/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        record User(int id, String name) {
+        }
+        var user = new User(1, "Pavel Yurchenko");
+        model.addAttribute("user", user);
         return "index";
     }
 }
