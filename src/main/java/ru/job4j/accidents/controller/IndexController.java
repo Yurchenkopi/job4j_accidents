008d956fc8e8ac5e79011674ba7de652e1411a32
Pavel <heppie@yandex.ru>
2024-07-28 12:30:49 +0300

    1. Загрузка данных в вид [#295782 #506853]

--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -3,14 +3,13 @@ package ru.job4j.accidents.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.accidents.model.User;
 
 @Controller
 public class IndexController {
     @GetMapping({"/", "/index"})
     public String getIndex(Model model) {
-        record User(int id, String name) {
-        }
-        var user = new User(1, "Pavel Yurchenko");
+        var user = new User(1, "Pavel Yurchenko", "login", "password");
         model.addAttribute("user", user);
         return "index";
     }
---
+++ src/main/java/ru/job4j/accidents/model/User.java
@@ -0,0 +1,22 @@
+package ru.job4j.accidents.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = {"id"})
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String login;
+
+    private String password;
+
+}
