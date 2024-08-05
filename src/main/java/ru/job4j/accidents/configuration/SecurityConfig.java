cf4591ad998ccaa135654c36c242803bf73332cb
Pavel <heppie@yandex.ru>
2024-08-05 22:49:45 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/configuration/SecurityConfig.java
+++ src/main/java/ru/job4j/accidents/configuration/SecurityConfig.java
@@ -1,32 +1,48 @@
 package ru.job4j.accidents.configuration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private final JdbcTemplate jdbc;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder)
-                .withUser("user").password(passwordEncoder.encode("123456")).roles("USER")
+        auth.jdbcAuthentication()
+                .dataSource(jdbc.getDataSource())
+                .withUser(User.withUsername("admin")
+                        .password(passwordEncoder().encode("123456"))
+                        .roles("ADMIN"))
                 .and()
-                .withUser("admin").password(passwordEncoder.encode("123456")).roles("USER", "ADMIN");
+                .jdbcAuthentication()
+                .dataSource(jdbc.getDataSource())
+                .withUser(User.withUsername("user")
+                        .password(passwordEncoder().encode("123456"))
+                        .roles("USER"));
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login", "/index")
+                .antMatchers("/login", "/index", "/css/**", "/js/**", "/umd/**")
                 .permitAll()
                 .antMatchers("/**")
                 .hasAnyRole("ADMIN", "USER")
--- src/main/java/ru/job4j/accidents/controller/IndexController.java
+++ src/main/java/ru/job4j/accidents/controller/IndexController.java
@@ -1,16 +1,18 @@
 package ru.job4j.accidents.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.accidents.model.User;
 
 @Controller
 public class IndexController {
     @GetMapping({"/", "/index"})
     public String getIndex(Model model) {
-        var user = new User(1, "Pavel Yurchenko", "login", "password");
-        model.addAttribute("user", user);
+        var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (!"anonymousUser".equals(principal)) {
+            model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        }
         return "index";
     }
 }
